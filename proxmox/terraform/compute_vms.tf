b0b8c2521a82b8d1274c89c2e4c1ddaa3bc15086
Nikk Nguyen <ndp190@gmail.com>
2025-07-12 15:44:28 +0700

    Update VM IDs

--- proxmox/terraform/compute_vms.tf
+++ proxmox/terraform/compute_vms.tf
@@ -2,7 +2,7 @@ resource "proxmox_vm_qemu" "compute" {
   name        = "compute"
   desc        = "Media VM for Docker (via SSH) and transcoding"
   agent       = 1
-  vmid        = 1101
+  vmid        = 102
   target_node = "pve"
   bios        = "seabios"
   clone       = "pkr-ubuntu-noble"
@@ -40,20 +40,7 @@ resource "proxmox_vm_qemu" "compute" {
   ciuser = "nikk"
   sshkeys = var.PUBLIC_SSH_KEY
 
-  # To install Docker and ffmpeg on first boot, upload a custom user-data file to your Proxmox node (e.g., /var/lib/vz/snippets/media-user-data.yaml)
+  # To install Docker and ffmpeg on first boot, upload a custom compute_vm_data file to your Proxmox node (e.g., scp ./compute_vm_data.yaml root@<PROXMOX_HOST>:/var/lib/vz/snippets/compute_vm_data.yaml)
   # Then add the following attribute to this resource:
-  cicustom = "user=local-lvm:snippets/media-user-data.yaml"
-}
-
-# Upload media-user-data.yaml to Proxmox snippets using Terraform
-resource "null_resource" "upload_media_userdata" {
-  provisioner "local-exec" {
-    command = "scp -o StrictHostKeyChecking=no -i /path/to/proxmox_id_rsa ${path.module}/compute_vm_data.yaml root@<PROXMOX_HOST>:/var/lib/vz/snippets/compute_vm_data.yaml"
-  }
-}
-
-# Make the VM depend on the upload
-resource "proxmox_vm_qemu" "media" {
-  # ... (existing config)
-  depends_on = [null_resource.upload_media_userdata]
+  cicustom = "user=local-lvm:snippets/compute_vm_data.yaml"
 }
