85fcc44bfc2f58eea09a5712c946229d41b193f0
Nikk Nguyen <ndp190@gmail.com>
2025-07-28 22:20:46 +0700

    Add ubuntu compute vm

--- proxmox/terraform/compute_vms.tf
+++ proxmox/terraform/compute_vms.tf
@@ -5,13 +5,31 @@ resource "proxmox_vm_qemu" "compute" {
   vmid        = 102
   target_node = "pve"
   bios        = "seabios"
-  clone       = "pkr-ubuntu-noble"
-  full_clone  = true
+  # clone       = "pkr-ubuntu-noble"
+  # full_clone  = true
   cores       = 16
   memory      = 16384
   sockets     = 1
   scsihw      = "virtio-scsi-pci"
   onboot      = true
+  iso         = "local:iso/ubuntu-24.04.2-desktop-amd64.iso"
+  boot        = "order=scsi0;ide2"
+
+  # Main Disk
+  disk {
+    type        = "scsi"
+    storage     = "local-lvm"
+    size        = "64G"
+    iothread    = 1
+  }
+
+  # Second Data Disk
+  disk {
+    type     = "scsi"
+    storage  = "hdd-vmstore"
+    size     = "128G"
+    iothread = 1
+  }
 
   network {
     model  = "virtio"
@@ -27,10 +45,10 @@ resource "proxmox_vm_qemu" "compute" {
   }
 
   # Cloud Init Settings
-  ipconfig0 = "ip=192.168.33.102/24,gw=192.168.33.1"
-  nameserver = "192.168.33.1"
-  ciuser = "nikk"
-  sshkeys = var.PUBLIC_SSH_KEY
+  # ipconfig0 = "ip=192.168.33.102/24,gw=192.168.33.1"
+  # nameserver = "192.168.33.1"
+  # ciuser = "nikk"
+  # sshkeys = var.PUBLIC_SSH_KEY
 
   # To install Docker and ffmpeg on first boot, upload a custom compute_vm_data file to your Proxmox node (e.g., scp ./compute_vm_data.yaml root@<PROXMOX_HOST>:/mnt/hdd-vmstore/snippets/compute_vm_data.yaml)
   # Then add the following attribute to this resource:
